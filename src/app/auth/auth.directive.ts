204ee271b8a713b032b17264fd8c8ab81b01bea2
marcapedoh <adralabel@gmail.com>
2024-11-28 14:25:38 +0000

    structural directive completed

--- src/app/auth/auth.directive.ts
+++ src/app/auth/auth.directive.ts
@@ -8,18 +8,18 @@ import { AuthService } from './auth.service';
 })
 export class AuthDirective {
 
-  userType = input.required<Permission>();
+  userType = input.required<Permission>({ alias: 'appAuth' });
 
   private authService = inject(AuthService);
-  private templateRef= inject(TemplateRef)
-  private viewContainerRef= inject(ViewContainerRef)
+  private templateRef = inject(TemplateRef)
+  private viewContainerRef = inject(ViewContainerRef)
 
   constructor() {
     effect(() => {
       if (this.authService.activePermission() === this.userType()) {
-       this.viewContainerRef.createEmbeddedView(this.templateRef)
-      }else{
-       this.viewContainerRef.clear()
+        this.viewContainerRef.createEmbeddedView(this.templateRef)
+      } else {
+        this.viewContainerRef.clear()
       }
     })
   }
